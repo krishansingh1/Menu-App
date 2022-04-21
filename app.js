70b5ce46878b6e6f4d9e54bfd252bc183913a731
krishansingh1 <kkrishan7074@gmail.com>
2022-04-21 18:20:19 +0530

    Commit

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-const all=document.getElementById('all')
+const all = document.getElementById('all');
+
+all.addEventListener('click', () => {
+    
+})
+
+// const all2=document.getElementById('price2')
+
